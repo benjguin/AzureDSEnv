b12576f23d7e6ede8a8129ff8f0eb4dc4ef2658c
@benjguin <bengui@microsoft.com>
2018-10-04 11:52:27 +0200

    deploy 1 Azure Data Science VM

---
+++ initvars.sh
@@ -0,0 +1,20 @@
+# This file is meant to be sourced as it only init variable values
+# usage: source initvars.sh
+
+subscription="Azure bengui"
+rg="a_180927a"
+suffix="eg123" # the suffix should make names that use it unique yet simple. This sample means "e.g. 123".
+location="westeurope"
+vNetIpRange="10.9.0.0/16"
+defaultSubnetIpRange="10.9.0.0/24"
+idsvmSubnetIpRange="10.9.1.0/24"
+dsvmSize="XS"
+adminUsername="benjguin"
+adminPassword="OVznpwjr88_____"
+artifactsStorageAccount="artifacts$suffix"
+artifactsStorageContainer="artifacts"
+
+# the following values are calculated, as in the ARM template
+dsvmName="dsvm$suffix"
+dsvmUrl="$dsvmName.$location.cloudapp.azure.com"
+
---
+++ setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/initvars.sh
+
+#login to azure using your credentials
+az account show 1> /dev/null
+
+if [ $? != 0 ];
+then
+	az login
+fi
+
+#set the default subscription
+az account set -s "$subscription"
+
+#Check for existing RG
+searchresult=`az group show -g $rg | wc -l`
+
+if [ "$searchresult" == "0" ]; then
+	echo "Resource group with name ${rg} could not be found. Creating new resource group.."
+	(
+		set -x
+		az group create --name ${rg} --location ${location} 1> /dev/null
+	)
+	else
+	echo "Using existing resource group..."
+fi
+
+#publish artifacts
+storageAccountExists=`az storage account list --query "[].name" --output tsv | grep $artifactsStorageAccount | wc -l`
+if [ "$storageAccountExists" == "0" ]; then
+	echo "Storage account \"$artifactsStorageAccount\" could not be found. Creating new storage account..."
+	az storage account create -g $rg --location $location --name $artifactsStorageAccount --sku Standard_LRS --https-only true --kind StorageV2
+else
+	echo "Using existing storage account \"$artifactsStorageAccount\""
+fi
+
+storageContainerExists=`az storage container exists --account-name $artifactsStorageAccount --name $artifactsStorageContainer --output tsv | grep True | wc -l`
+if [ "$storageContainerExists" == "0" ]; then
+	echo "Storage container \"$artifactsStorageContainer\" could not be found. Creating new container..."
+	az storage container create --account-name $artifactsStorageAccount --name $artifactsStorageContainer --public-access off
+else
+	echo "Using existing container \"$artifactsStorageContainer\""
+fi
+
+az storage blob upload-batch --destination $artifactsStorageContainer --source "$DIR/artifacts/" --account-name $artifactsStorageAccount
+
+policyExpirationYear=$((`date +%Y`+2))
+policyExpirationDateTime="$policyExpirationYear-`date +%m-%d`T23:59:59Z"
+az storage container policy create --name "artifactsreadonly" --container-name "$artifactsStorageContainer" --account-name "$artifactsStorageAccount" --expiry "$policyExpirationDateTime" --permissions "r"
+artifactsSas=`az storage container generate-sas --name $artifactsStorageContainer --account-name $artifactsStorageAccount --policy-name "artifactsreadonly" --output tsv`
+artifactsPrefix="https://${artifactsStorageAccount}.blob.core.windows.net/${artifactsStorageContainer}/"
+artifactsSuffix="?${artifactsSas}"
+
+deploymentName="setup_starting_`date +%y%m%d_%HH%MM%SS.%N`"
+
+#Start deployment
+echo "Starting deployment \"$deploymentName\" ..."
+
+az group deployment create --name "$deploymentName" --resource-group "$rg" \
+	--template-file "$DIR/template.json" \
+	--parameters uniquesuffix="$suffix" \
+	--parameters tshirtSize="$dsvmSize" \
+	--parameters location="$location" \
+	--parameters vNetIpRange="$vNetIpRange" \
+	--parameters defaultSubnetIpRange="$defaultSubnetIpRange" \
+	--parameters idsvmSubnetIpRange="$idsvmSubnetIpRange" \
+	--parameters dsvmAdminUsername="$adminUsername" \
+	--parameters dsvmAdminPassword="$adminPassword" \
+	--parameters artifactsPrefix="$artifactsPrefix" \
+	--parameters artifactsSuffix="$artifactsSuffix"
+
+if [ $?  == 0 ];
+then
+	echo "Template has been successfully deployed"
+else
+	echo "Template was NOT successfully deployed"
+	exit 1
+fi
+
+echo "you should be able to ssh to ${adminUsername}@${dsvmUrl}"
